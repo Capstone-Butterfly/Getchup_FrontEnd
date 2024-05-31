bc7978e0750fae591b2f7586452cf884002820c4
Maria Beatriz Camargo <mbeatrizcamargo@hotmail.com>
2024-05-30 22:33:43 -0700

    build: install gluestack

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react'; import { Image } from '@gluestack-ui/themed';
+
+import { GluestackUIProvider } from '@gluestack-ui/themed';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+    return <GluestackUIProvider>
+        <View style={styles.container}>
+            <Text>Welcome to Getchup 🍅</Text>
+            <StatusBar style="auto" />
+        </View>
+    </GluestackUIProvider>;
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
