0f23ed02d957c335a8eca2567d0b9ca6fc1ebfd4
harleen251 <h251@mylangara.ca>
2024-06-11 16:23:05 -0700

    BC-115: implement authentication and authorization calls on sample screens

---
+++ src/utils/authUtils.js
@@ -0,0 +1,33 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {jwtDecode} from 'jwt-decode';
+
+export const checkAuthorization = async (navigation) => {
+  try {
+    const token = await AsyncStorage.getItem('authToken');
+    console.log("Token inside authUtils is", token);
+
+    if (token) {
+      // Token exists, perform validation
+      console.log("Inside if statement");
+      const decodedToken = jwtDecode(token);
+      console.log("Decoded token inside authUtils is", decodedToken);
+      const currentTime = Date.now() / 1000; // Convert to seconds
+
+      if (decodedToken.exp < currentTime) {
+        // Token is expired, navigate to login screen
+        navigation.navigate('Login');
+        return false;
+      } else {
+        // Token is valid
+        return true;
+      }
+    } else {
+      // Token does not exist, navigate to login screen
+      navigation.navigate('Login');
+      return false;
+    }
+  } catch (error) {
+    console.error('Error checking token validity:', error);
+    return false;
+  }
+};
