eff4d91f79d25cf5a2ab0800553f5949144f8580
greeshma017 <gdharmapalan00@mylangara.ca>
2024-06-18 21:59:34 -0700

    feat: BC-175 add create task store

---
+++ src/store/createTaskStore.js
@@ -0,0 +1,35 @@
+import { create } from "zustand";
+
+const useCreateTaskStore = create((set) => ({
+    title: "",
+    date: "",
+    is_repeated: false,
+    notes: "",
+    task_urgency: "medium",
+    subTasks: [],
+  
+    // Setters
+    setTitle: (title) => set({ title }),
+    setDate: (date) => set({ date }),
+    setIsRepeated: (is_repeated) => set({ is_repeated }),
+    setNotes: (notes) => set({ notes }),
+    setTaskUrgency: (task_urgency) => set({ task_urgency }),
+  
+    // Setters for subtasks
+    addSubtask: (subtask) =>
+      set((state) => ({
+        subtasks: [...state.subtasks, subtask],
+      })),
+    updateSubtask: (index, updatedSubtask) =>
+      set((state) => ({
+        subtasks: state.subtasks.map((subtask, i) =>
+          i === index ? updatedSubtask : subtask
+        ),
+      })),
+    removeSubtask: (index) =>
+      set((state) => ({
+        subtasks: state.subtasks.filter((_, i) => i !== index),
+      })),
+  }));
+
+  export { useCreateTaskStore };
--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,22 @@
-import {create} from 'zustand';
-import axios from 'axios';
+import { create } from "zustand";
+import axios from "axios";
 
 const useTaskStore = create((set) => ({
-    tasks: [],
-    loading: true,
-    error: null,
-    selectedDate: new Date(),
-    fetchTasks: async (userId) => {
-        try {
-            const response = await axios.get('http://52.55.48.104:8080/api/v1/tasks');
-            const userTasks = response.data.filter(task => task.user_id === userId);
-            set({ tasks: userTasks, loading: false, error: null });
-        } catch (error) {
-            set({ error, loading: false });
-        }
-    },
-    setSelectedDate: (date) => set({ selectedDate: date })
+  tasks: [],
+  loading: true,
+  error: null,
+  selectedDate: new Date(),
+  fetchTasks: async (userId) => {
+    try {
+      const response = await axios.get("http://52.55.48.104:8080/api/v1/tasks");
+      const userTasks = response.data.filter((task) => task.user_id === userId);
+      set({ tasks: userTasks, loading: false, error: null });
+    } catch (error) {
+      set({ error, loading: false });
+    }
+  },
+  setSelectedDate: (date) => set({ selectedDate: date }),
 }));
 
-export default useTaskStore;
+
+export default useTaskStore ;
