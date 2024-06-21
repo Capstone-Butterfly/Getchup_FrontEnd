8e40aa312986ac54fb3ae6d2386b2742a99f5ae8
Tin Zar Paing <tinzarpaing83@gmail.com>
2024-06-21 02:08:12 -0700

    feat: edit add new task by AI - V1

--- src/screens/add-task/AddTaskScreen.jsx
+++ src/screens/add-task/AddTaskScreen.jsx
@@ -1,26 +1,43 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { AddIcon, Box, Button, ButtonIcon, ButtonText, Card, Center, FlatList, HStack, Heading, Text } from "@gluestack-ui/themed"
+import { AddIcon, Box, Button, ButtonIcon, ButtonText, Card, Center, FlatList, HStack, Heading, Icon, Input, InputField, InputIcon, InputSlot, Pressable, Text, TextareaInput } from "@gluestack-ui/themed"
 import useTaskStore from "../../store/taskStore"
 import { useEffect } from 'react';
-
+import { CalendarDaysIcon } from "@gluestack-ui/themed";
+import { addTask } from '../../services/tasks'
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 const AddTaskScreen = ({ navigation }) => {
-    const { tasks, setTasks, selectedDate, setSelectedDate } = useTaskStore((state) => ({
-        tasks: state.tasks,
-        setTasks: state.setTasks,
-        selectedDate: state.selectedDate,
-        setSelectedDate: state.setSelectedDate
-    }));
-    
+    const { tasks, addDataTask } = useTaskStore();
     const storedTasks = tasks;
-    
-    useEffect(() => {
-        if (storedTasks){
-            console.log("storedTasks in AddTaskScreen : ", storedTasks.length + "  " + new Date().toString());
-            console.log("selectedDate in AddTaskScreen : ", selectedDate.toString());
-        }
-      }, [tasks, selectedDate]);
+    const title = 'Clean my desk';
+    const userId = '6668b7f95dbce97bc28322d2';
+    const queryClient = useQueryClient();
 
+    const mutation = useMutation(
+    {
+        mutationFn: async (newTask) => await addTask(newTask),
+        onSuccess: (data) => {
+        queryClient.invalidateQueries(['tasks']);
+        displayData(data);
+        },
+    });
+
+    const displayData = (data) => {
+        console.log("New Data ", JSON.stringify(data));
+    }
+
+    useEffect(() => {
+        console.log("TASKS :", tasks);
+    }, [tasks]);
+
+    const addToTask = async () => {
+    try {
+        const newTask = { title: title, user_id: userId };
+        await mutation.mutateAsync(newTask);
+    } catch (error) {
+        console.error('Error adding task:', error);
+    }
+    };
     
 
     return (
@@ -44,13 +61,44 @@ const AddTaskScreen = ({ navigation }) => {
             </Box>
             </HStack>
             <Card>
-                <Button size="md" variant="link" action="primary" isDisabled={false} isFocusVisible={false} >
-                    <ButtonText>Add </ButtonText>
-                </Button>
-                <Button size="md" variant="link" action="primary" isDisabled={false} isFocusVisible={false} >
-                    <ButtonText>Date </ButtonText>
-                </Button>
-            
+                <Pressable
+                    onPress={() => console.log('Title')} p="$5">
+                    <Text color="black" fontWeight={"$bold"}>Add Task Title</Text>
+                    <Input variant="underlined" size="md" isDisabled={false} isInvalid={false} isReadOnly={false} >
+                    <InputField
+                        placeholder='Enter Text here'
+                        value={title}
+                    />
+                    {/* <InputSlot>
+                        <InputIcon as={AddIcon}></InputIcon>
+                    </InputSlot> */}
+                    </Input>
+                </Pressable>
+                <Pressable
+                    onPress={() => console.log('Date')} p="$5">   
+                    <Icon as={CalendarDaysIcon} stroke={'black'} fill={'black'} style={{ color: 'black' }}   size="md" />
+                    <Text color="black" fontWeight={"$bold"}>Date</Text>
+                </Pressable>
+                <Pressable
+                    onPress={() => console.log('Notes')} p="$5"> 
+                    <Icon as={CalendarDaysIcon} stroke={'black'} fill={'black'} style={{ color: 'black' }}   size="md" />
+                    <Text color="black" fontWeight={"$bold"}>Notes</Text>
+                </Pressable>
+                <Pressable
+                    onPress={() => console.log('Task priority')} p="$5">
+                    <Icon as={CalendarDaysIcon} stroke={'black'} fill={'black'} style={{ color: 'black' }}   size="md" />
+                    <Text color="black" fontWeight={"$bold"}>Task priority</Text>
+                </Pressable>
+            </Card>
+            <Card>
+            <Pressable
+                onPress={() => console.log('Title')} p="$5">
+                <Text color="black" fontWeight={"$bold"}>Add subtask</Text>
+            </Pressable>
+            <Text>OR</Text>
+            <Button size="md" variant="solid" action="primary" isDisabled={false} isFocusVisible={false} onPress={addToTask} >
+                <ButtonText verticalAlign="middle"> Add Subtask by AI </ButtonText>
+            </Button>
             </Card>
         </SafeAreaView>
     );
