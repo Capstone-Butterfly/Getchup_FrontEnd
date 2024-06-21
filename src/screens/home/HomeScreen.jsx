3e6081f18b942a7f74284e8f55f07e348aff885b
harleen251 <h251@mylangara.ca>
2024-06-21 08:59:38 -0700

    BC-164: implement focus mode screen and add movement tracking using accelerometer

--- src/screens/home/HomeScreen.jsx
+++ src/screens/home/HomeScreen.jsx
@@ -3,7 +3,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../../components/Header";
 import Greeting from "../../components/Greeting";
 import WeeklyCalendar from "../../components/WeeklyCalendar2";
-import { Text } from "@gluestack-ui/themed";
+import { Button, ButtonText, Text } from "@gluestack-ui/themed";
 
 const userId = '6668b7f95dbce97bc28322d2';
 
@@ -13,6 +13,9 @@ const HomeScreen = ({ navigation }) => {
             <Header />
             <Greeting />
             <WeeklyCalendar userId={userId} />
+            <Button onPress={() => navigation.navigate('FocusModeScreen')}>
+                <ButtonText>Go to Focus Mode</ButtonText>
+            </Button>
         </SafeAreaView>
     );
 };
--- src/stacks/HomeStack.jsx
+++ src/stacks/HomeStack.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/home/HomeScreen';
+import FocusModeScreen from '../screens/focus-mode/FocusModeScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -8,6 +9,7 @@ const HomeStack = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="FocusModeScreen" component={FocusModeScreen} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
 }
