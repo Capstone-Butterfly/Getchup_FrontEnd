21b435bd7c87fa32f295e0af75ca38b35fc1462c
greeshma017 <gdharmapalan00@mylangara.ca>
2024-06-26 14:34:38 -0700

    feat: BC-185 add time model screen

--- src/screens/add-task/DateTimeModelScreen.js
+++ src/screens/add-task/DateTimeModelScreen.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import AddTaskDateContainer from "../../components/AddTaskDateContainer";
+import AddTaskTimeContainer from "../../components/AddTaskTimeContainer";
 
 const DateTimeModelScreen = () => {
   return (
     <SafeAreaView>
       <Text style={styles.title}>Date and Time</Text>
       <AddTaskDateContainer />
+      <AddTaskTimeContainer />
     </SafeAreaView>
   );
 };
