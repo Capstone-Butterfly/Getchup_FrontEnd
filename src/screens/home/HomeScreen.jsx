c692d96e8cccacd00d1285434e130a7379ab5fc1
Maria Beatriz Camargo <mbeatrizcamargo@hotmail.com>
2024-06-26 00:46:15 -0700

    feat: basic local notification implemented

--- src/screens/home/HomeScreen.jsx
+++ src/screens/home/HomeScreen.jsx
@@ -3,35 +3,59 @@ import { Button, SafeAreaView, Text, View, Platform } from 'react-native';
 import Header from '../../components/Header';
 import Greeting from '../../components/Greeting';
 import WeeklyCalendar from '../../components/WeeklyCalendar2';
-import { registerForPushNotificationsAsync, sendNotification } from '../../services/notificationService';
 import { BASE_URL } from '../../config/apiConfig';
+import { useRef } from 'react';
+import * as Notifications from 'expo-notifications';
+import { registerForPushNotificationsAsync, schedulePushNotification } from '../../services/notificationService';
 
-const userId = '667a15822ecae2cb2e981c78';
+const userId = '6668b7f95dbce97bc28322d2';
 const base_url = BASE_URL
 
+const trigger = 2;
+
 const HomeScreen = ({ navigation }) => {
-  const [expoPushToken, setExpoPushToken] = useState('');
 
-  useEffect(() => {
-    console.log('Registering for push notifications');
-    registerForPushNotificationsAsync().then(token => {
-      console.log('token: ', token);
-      setExpoPushToken(token);
-    }).catch(err => {
-      console.log(err);
-    });
-  }, []);
+    const [expoPushToken, setExpoPushToken] = useState('');
+    const [channels, setChannels] = useState([]);
+    const [notification, setNotification] = useState(undefined);
+    const notificationListener = useRef();
+    const responseListener = useRef();
 
-  registerForPushNotificationsAsync()
+    useEffect(() => {
+        registerForPushNotificationsAsync().then(token => token && setExpoPushToken(token));
 
-  return (
-    <SafeAreaView>
-      <Header />
-      <Greeting />
-      <WeeklyCalendar userId={userId} />
-      <Button title="Send push notification" onPress={() => sendNotification(userId)} />
-    </SafeAreaView>
-  );
+        if (Platform.OS === 'android') {
+            Notifications.getNotificationChannelsAsync().then(value => setChannels(value ?? []));
+        }
+
+        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+            setNotification(notification);
+        });
+
+        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+            console.log(response);
+        });
+
+        return () => {
+            if (notificationListener.current) {
+                Notifications.removeNotificationSubscription(notificationListener.current);
+            }
+            if (responseListener.current) {
+                Notifications.removeNotificationSubscription(responseListener.current);
+            }
+        };
+    }, []);
+
+    return (
+        <SafeAreaView>
+            <Header />
+            <Greeting />
+            <WeeklyCalendar userId={userId} />
+            <Button title="Send push notification" onPress={async () => { await schedulePushNotification('Hello there', 'General Kenobi', trigger); }} />
+        </SafeAreaView>
+    );
 };
+
+
 
 export default HomeScreen;
--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -15,94 +15,62 @@ Notifications.setNotificationHandler({
     }),
   });
 
-  const fetchUserPushToken = async (userId) => {
-    try {
-      const response = await axios.get(`${base_url}/getUserDetails/${userId}`);
-      if (response.status !== 200) {
-        throw new Error('Failed to fetch user push token');
-      }
-
-      const push_token = response.data.user.push_token
-      console.log(response.data.user.push_token);
-      return push_token;
-    } catch (error) {
-      console.error('Error fetching user push token:', error);
-    }
-  };
-
-  const sendNotification = async (userId) => {
-    try {
-      const pushToken = await fetchUserPushToken(userId);
-      console.log('Sending push notification...');
-
-      const message = {
-        to: pushToken,
-        sound: 'default',
-        title: 'My first push notification',
-        body: 'This is my first push notification made with expo',
-      };
-
-      await fetch('https://exp.host/--/api/v2/push/send', {
-        method: 'POST',
-        headers: {
-          host: 'exp.host',
-          accept: 'application/json',
-          'accept-encoding': 'gzip, deflate',
-          'content-type': 'application/json',
+  async function schedulePushNotification(title, body, trigger) {
+    const scheduledNotificationId = await Notifications.scheduleNotificationAsync({
+        content: {
+            title: title,
+            body: body,
+            data: { data: 'goes here', test: { test1: 'more data' } },
         },
-        body: JSON.stringify(message),
-      });
-    } catch (error) {
-      console.error('Error sending notification:', error);
-    }
-  };
-
+        trigger: { seconds: trigger },
+    });
+    console.log(scheduledNotificationId)
+    return scheduledNotificationId
+  }
+  
   async function registerForPushNotificationsAsync() {
     let token;
-
+  
     if (Platform.OS === 'android') {
-      await Notifications.setNotificationChannelAsync('default', {
-        name: 'default',
-        importance: Notifications.AndroidImportance.MAX,
-        vibrationPattern: [0, 250, 250, 250],
-        lightColor: '#FF231F7C',
-      });
+        await Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C',
+        });
     }
-
-    if (Platform.OS === 'ios') {
-      const { status: existingStatus } = await Notifications.getPermissionsAsync();
-      let finalStatus = existingStatus;
-      if (existingStatus !== 'granted') {
-        const { status } = await Notifications.requestPermissionsAsync();
-        finalStatus = status;
-      }
-      if (finalStatus !== 'granted') {
-        alert('Failed to get push token for push notification!');
-        return;
-      }
-    }
-
+  
     if (Device.isDevice) {
-      try {
-        const projectId =
-          Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
-        if (!projectId) {
-          throw new Error('Project ID not found');
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
         }
-        token = (
-          await Notifications.getExpoPushTokenAsync({
-            projectId,
-          })
-        ).data;
-        console.log(token);
-      } catch (e) {
-        token = `${e}`;
-      }
+        if (finalStatus !== 'granted') {
+            alert('Failed to get push token for push notification!');
+            return;
+        }
+        try {
+            const projectId =
+                Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+            if (!projectId) {
+                throw new Error('Project ID not found');
+            }
+            token = (
+                await Notifications.getExpoPushTokenAsync({
+                    projectId,
+                })
+            ).data;
+            console.log(token);
+        } catch (e) {
+            token = `${e}`;
+        }
     } else {
-      alert('Must use physical device for Push Notifications');
+        alert('Must use physical device for Push Notifications');
     }
-
+  
     return token;
   }
 
-export { fetchUserPushToken, sendNotification, registerForPushNotificationsAsync };
+export { schedulePushNotification, registerForPushNotificationsAsync };
