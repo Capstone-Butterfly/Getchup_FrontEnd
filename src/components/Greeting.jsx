bae6c04bd710ab2d2a1650bea20bcae65ecfd80d
Tin Zar Paing <tinzarpaing83@gmail.com>
2024-07-03 15:58:13 -0700

    feat: resolve conflicts

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -5,25 +5,23 @@ import { StyleSheet } from "react-native";
 import { defaultStyles } from "../styles/styles";
 
 const Greeting = ({name}) => {
-    const userId = '6668b7f95dbce97bc28322d2';
-    const firstName = 'Nam';
     const [greeting, setGreeting] = useState('');
 
     useEffect(() => {
-        const getGreetingMessage = (firstName) => {
+        const getGreetingMessage = (name) => {
             const currentHour = new Date().getHours();
             if (currentHour >= 5 && currentHour < 12) {
-                return `Good morning, ${firstName}!\nRise up, start fresh!`;
+                return `Good morning, ${name}!\nRise up, start fresh!`;
             } else if (currentHour >= 12 && currentHour < 17) {
-                return `Good afternoon, ${firstName}!\nKeep going, you're halfway to your goal!`;
+                return `Good afternoon, ${name}!\nKeep going, you're halfway to your goal!`;
             } else if (currentHour >= 17 && currentHour < 21) {
-                return `Good evening, ${firstName}!\nRelax and recharge`;
+                return `Good evening, ${name}!\nRelax and recharge`;
             } else {
-                return `Good night, ${firstName}!\nRest well, for tomorrow brings new chances`;
+                return `Good night, ${name}!\nRest well, for tomorrow brings new chances`;
             }
         };
 
-        setGreeting(getGreetingMessage(firstName));
+        setGreeting(getGreetingMessage(name));
     }, []);
 
     return (
