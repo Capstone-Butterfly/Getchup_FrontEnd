faea852e9ed8232f30c946793d9de0a3cecd8e1a
Your GitHub Username <farhangalvandy@gmail.com>
2024-07-11 12:25:02 -0700

    calendar components and screen are styled

--- src/screens/calendar/CalendarScreen.jsx
+++ src/screens/calendar/CalendarScreen.jsx
@@ -1,16 +1,49 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Text } from "@gluestack-ui/themed"
-import Header from "../../components/Header";
+import { Center, Heading, ImageBackground, Text, View } from "@gluestack-ui/themed"
 import MonthlyCalendar from "../../components/Calendar";
+import { StyleSheet, Dimensions } from "react-native";
+import { config } from '../../styles/themeConfig'; 
+import { defaultStyles } from './../../styles/styles';
+const image = require('../../../assets/background/background.png');
 
-
+// Get device dimensions
+const { width, height } = Dimensions.get('window');
 const CalendarScreen = ({ navigation }) => {
     return (
         <SafeAreaView>
-            <Header />
+                        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+<View style={[styles.calendarContainer]}>
+            <View style={[styles.calendarHeader, defaultStyles.TypographyH1]}>
+            <Heading style={[styles.heading, defaultStyles.TypographyH1]}>
+            Calendar
+          </Heading>
+          <View><Text>Today, Jun 19</Text></View>
+          </View>
+            </View>
             <MonthlyCalendar navigation={navigation}/>
+           
+            </ImageBackground>
         </SafeAreaView>
     );
 };
+
+
+const styles = StyleSheet.create({
+    heading: {
+        textAlign: 'left',
+        marginBottom: config.tokens.spacing.md,
+      },
+      calendarHeader:{
+        paddingVertical:20
+      },
+      calendarContainer:{
+        width: width * 0.9, 
+        maxWidth: 400, 
+        minWidth: 300, 
+        marginVertical:0,
+        marginHorizontal:'auto'
+      }
+  });
+  
 
 export default CalendarScreen;
