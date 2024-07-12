250dc92dad05feb85709fde697a49a1cae42b305
Tin Zar Paing <tinzarpaing83@gmail.com>
2024-07-11 23:57:04 -0700

    feat: fix add KeyboardAvoidingView to subTask and notes modal

--- src/screens/add-task/NotesModelScreen.js
+++ src/screens/add-task/NotesModelScreen.js
@@ -1,23 +1,52 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { TextInput, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import NotesInputForm from "../../components/NotesInputForm";
+import usecreateTaskStore from '../../store/createTaskStore';
+import { config } from '../../styles/themeConfig'; // Import the theme configuration
+import { defaultStyles } from './../../styles/styles'
+import { Card, View } from "@gluestack-ui/themed";
 
 const NotesModelScreen = () => {
+  const { notes, setNotes } = usecreateTaskStore((state) => ({
+    notes: state.notes,
+    setNotes: state.setNotes,
+  }));
+  
   return (
-    <SafeAreaView>
-      {/* <Text style={styles.title}>Notes</Text> */}
-      <NotesInputForm />
-    </SafeAreaView>
+    <>
+      {/* <NotesInputForm /> */}
+      <Card style={[defaultStyles.card, styles.card]}>
+      <View style={styles.container}>
+        <TextInput
+          style={[defaultStyles.TypographyH2, styles.input]}
+          placeholder="Add notes"
+          placeholderTextColor={config.tokens.colors.lighterBlack}
+          value={notes}
+          onChangeText={setNotes}
+          multiline={true}
+          numberOfLines={2}
+          textAlignVertical="top"
+        />
+      </View>
+    </Card>
+    </>
   );
 };
 
 export default NotesModelScreen;
 
 const styles = StyleSheet.create({
-  title: {
-    fontWeight: "bold",
-    fontSize: 24,
-    textAlign: "center",
+  container: {
+    margin: 5,
+  },
+  card: {
+    borderRadius: 20, 
+  },
+  input: {
+    width: 295,
+    height: 40,
+    borderBottomColor: config.tokens.colors.lighterBlack,
+    borderBottomWidth: 1,
+    paddingHorizontal: 10,
   },
 });
